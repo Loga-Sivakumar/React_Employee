8f5864d4ba1c70ef6dc9347e0b790e09759d2a52
Loga-Sivakumar <60094337+Loga-Sivakumar@users.noreply.github.com>
2020-03-03 12:47:14 +0530

    Update EmployeeList.jsx

--- employees-management-console/src/components/EmployeeList.jsx
+++ employees-management-console/src/components/EmployeeList.jsx
@@ -1,46 +1,49 @@
-import React from "react";
-import EditEmployee from "./EditEmployee";
+import React from 'react';
 const EmployeeList = props => {
   const employeeNames = props.employeeslist;
   console.log(
-    "inside Employee List " + employeeNames.map(emp => emp.employee_name)
+    'inside Employee List ' + employeeNames.map(emp => emp.employee_name)
   );
+  
 
   return (
     <React-Fragment>
       <br></br>
       <br></br>
-      <h5 className="regform">
-        The employees in the organization are as follows....
+      <h5 className='regform'>
+        Here are the employees of your organization!!!
       </h5>
       <ul>
         {employeeNames.map(emp => (
           <div key={emp.id}>
-            {emp.employee_name}
+            {emp.employee_name} 
+            <div>
             <button
-              className="btn btn-primary btn-sm m-2"
+              className='btn btn-primary btn-sm m-2'
               onClick={() => {
                 props.onEdit(
                   emp.id,
                   emp.employee_name,
                   emp.employee_age,
-                  emp.employee_salary
+                  emp.employee_salary,
                 );
               }}
             >
               Edit
             </button>
             <button
-              className="btn btn-danger btn-sm m-2"
+              className='btn btn-danger btn-sm m-2'
               onClick={() => {
                 if (
-                  window.confirm("Are you sure you want to delete this item?")
+                  window.confirm(`Are you sure you want to delete ${emp.employee_name} ?`)
                 )
                   props.onDelete(emp.id);
               }}
             >
               Delete
             </button>
+            <br></br><br></br>
+            </div>
           </div>
         ))}
       </ul>
