0c46d677f6beb2e19ead9cac891c469daf216bbb
Loga-Sivakumar <60094337+Loga-Sivakumar@users.noreply.github.com>
2020-03-03 15:03:00 +0530

    Update index.js

--- employees-management-console/src/index.js
+++ employees-management-console/src/index.js
@@ -1,11 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import Employee from "./components/employee";
-ReactDOM.render(<Employee />, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router,Switch } from 'react-router-dom';
+import Employee from './components/employee';
+import pageNotFound from './components/pageNotFound';
+import './index.css';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+const routing = ( <Router>
+    <Switch>
+<Route exact path = '/employees' component = {Employee}/>
+<Route  component = {pageNotFound}/>
+</Switch>
+</Router>
+ );
+ReactDOM.render(routing, document.getElementById('root'));
 serviceWorker.unregister();
